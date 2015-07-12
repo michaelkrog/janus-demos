1dcf0ecb4863e628cafd09193c66626bf34e12a7
Michael Krog <mic@apaq.dk>
2015-07-12 18:40:31 +0200

    Update streamingtest.js

--- streamingtest.js
+++ streamingtest.js
@@ -143,7 +143,7 @@ $(document).ready(function() {
 									console.log(" ::: Got a remote stream :::");
 									console.log(JSON.stringify(stream));
 									if($('#remotevideo').length === 0)
-										$('#stream').append('<video class="rounded centered hide" id="remotevideo" width=640 height=480 autoplay/>');
+										$('#stream').append('<video class="rounded centered hide" id="remotevideo" width=320 height=240 autoplay/>');
 									// Show the stream and hide the spinner when we get a playing event
 									$("#remotevideo").bind("playing", function () {
 										$('#waitingvideo').remove();
@@ -220,7 +220,7 @@ function startStream() {
 	var body = { "request": "watch", id: parseInt(selectedStream) };
 	streaming.send({"message": body});
 	// No remote video yet
-	$('#stream').append('<video class="rounded centered" id="waitingvideo" width=640 height=480 />');
+	$('#stream').append('<video class="rounded centered" id="waitingvideo" width=320 height=240 />');
 	if(spinner == null) {
 		var target = document.getElementById('stream');
 		spinner = new Spinner({top:100}).spin(target);
